b150776438911b9cb5afaff44eb6b2d164ed0752
merahul1992 <90108415+merahul1992@users.noreply.github.com>
2024-09-13 18:46:41 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Spacewar.mk
+    $(LOCAL_DIR)/superior_Spacewar.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_Spacewar-userdebug \
-    lineage_Spacewar-user \
-    lineage_Spacewar-eng
+    superior_Spacewar-userdebug \
+    superior_Spacewar-user \
+    superior_Spacewar-eng
